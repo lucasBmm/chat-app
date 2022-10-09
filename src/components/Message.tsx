88989039f3e7ee83ae35505c05dea169029a24ba
lucasBmm <lucassbomfimm@gmail.com>
2022-10-09 14:43:48 -0300

    firebase project created

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export const Message: React.FC = (): JSX.Element => {
     return (
-        <div className='message owner'>
+        <div className='message'>
             <div className="message-info">
                 <img src="/images/userImage.jpg" alt="user image" />
                 <span>just now</span>
             </div>
             <div className="message-content">
                 <p>hello</p>
-                <img src="/images/userImage.jpg" alt="" />
+                {/* <img src="/images/userImage.jpg" alt="" /> */}
             </div>
         </div>
     )
---
+++ src/firebase.tsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNBjXWP1WT_oK9zXyKeXRZN6pp3FQmEl4",
+  authDomain: "chat-app-76337.firebaseapp.com",
+  projectId: "chat-app-76337",
+  storageBucket: "chat-app-76337.appspot.com",
+  messagingSenderId: "661456003182",
+  appId: "1:661456003182:web:061183b5092de03147067f",
+  measurementId: "G-QSWMYHNBZ2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const analytics = getAnalytics(app);
