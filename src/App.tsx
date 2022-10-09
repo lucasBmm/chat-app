9783b95aeb75279b029ebfb68f034b2c3349ffcf
lucasBmm <lucassbomfimm@gmail.com>
2022-10-09 15:51:37 -0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { Register } from './pages/Register/Register';
 import './style.scss'
 import { Login } from './pages/Login/Login';
 import { Home } from './pages/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={<Home />} />
+          <Route path='login' index element={<Login />} />
+          <Route path='register' index element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
