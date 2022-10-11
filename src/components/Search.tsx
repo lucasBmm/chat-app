3c0ce5a6bba2415ab62d70db6d1408e06845585c
lucasBmm <lucassbomfimm@gmail.com>
2022-10-11 20:00:54 -0300

    search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { collection, DocumentData, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export const Search: React.FC = (): JSX.Element => {
+    const [ username, setUsername ] = useState<string>("");
+    // FIXME: ADD CORRECT TYPES TO USER
+    const [ user    , setUser     ] = useState<any>();
+    const [ err     , setErr      ] = useState<boolean>(false);
+
+    const handleSearch = async () => {
+        const q = query(
+            collection(db, "users"), 
+            where("displayName", "==", username)
+        );
+
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data());
+            });
+        } catch (err) {
+            setErr(true);
+        }
+    }
+
+    const handleKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        e.code === "Enter" && handleSearch();
+    }
+
     return (
         <div className='search'>
             <div className="search-form">
-                <input type="text" placeholder='Find a user'/>
+                <input 
+                    type="text" 
+                    placeholder='Find a user' 
+                    onKeyDown={handleKey}
+                    onChange={e => setUsername(e.target.value)}
+                />
             </div>
-            <div className="user-chat">
-                <img src="/images/userImage.jpg" alt="" />
+            {err && <span>Something went wrong!</span>}
+            {user && <div className="user-chat">
+                <img src={user.photoURL} alt="" />
                 <div className="user-chat-info">
-                    <span>Jane</span>
+                    <span>{ user.displayName }</span>
                     <p>Hello</p>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
