d0f9d38836bd0c60c3922f45f63ae496efd05def
lucasBmm <lucassbomfimm@gmail.com>
2022-10-12 11:54:41 -0300

    Chats

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import { onSnapshot, doc } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { useEffect, useContext } from 'react';
+import { db } from './../firebase';
+import { AuthContext } from './../context/AuthContext';
+import { Unsubscribe } from 'firebase/auth';
 
 export const Chats: React.FC = (): JSX.Element => {
+
+    const [ chats, setChats ] = useState([]);
+
+    const currentUser = useContext(AuthContext);
+
+    useEffect(() => {
+        let unsub!: Unsubscribe;
+        
+        const getChats = () => {
+            console.log("get chats")
+            if (!currentUser) return;
+            unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                console.log( doc.data())
+            })
+
+            return () => {
+                unsub();
+            }
+        };
+        
+        console.log(currentUser)
+        getChats();
+
+    }, [currentUser?.uid])
+
+    console.log(Object.entries(chats));
+
     return (
         <>
             <div className="user-chat">
